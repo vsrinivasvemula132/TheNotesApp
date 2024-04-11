7ead255ec5f5e26059cc5ec69a7ef1e5e88704c3
vemula.srinivas <cometothere322@gmail.com>
2024-04-11 11:19:18 +0530

    Small changes2.12

--- app/src/main/java/com/example/thenotesapp/repository/NoteRepository.kt
+++ app/src/main/java/com/example/thenotesapp/repository/NoteRepository.kt
@@ -1,14 +1,24 @@
 package com.example.thenotesapp.repository
 
+import androidx.room.Query
 import com.example.thenotesapp.database.NoteDatabase
 import com.example.thenotesapp.model.Note
 
 class NoteRepository(private val db: NoteDatabase) {
 
-    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
-    suspend fun deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
-    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
-
-    fun getAllNotes() = db.getNoteDao().getAllNotes()
-    fun searchNote(query: String?) = db.getNoteDao().searchNote(query)
+    suspend fun insertNote(note: Note){
+        db.getNoteDao().insertNote(note)
+    }
+    suspend fun deleteNote(note: Note){
+        db.getNoteDao().deleteNote(note)
+    }
+    suspend fun updateNote(note: Note){
+        db.getNoteDao().updateNote(note)
+    }
+    fun getAllNotes(){
+        db.getNoteDao().getAllNotes()
+    }
+    fun searchNote(query: String?){
+        db.getNoteDao().searchNote(query)
+    }
 }
